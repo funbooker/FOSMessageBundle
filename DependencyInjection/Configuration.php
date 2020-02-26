c304b5e3d087a5f80cd4950480998f76ac9ec8a8
Eric Abouaf <eric.abouaf@gmail.com>
2020-02-26 23:24:38 +0100

    Fixing SF4.3+ deprecation notice for `TreeBuilder`

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,12 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('fos_message');
-        $rootNode = $treeBuilder->root('fos_message');
+
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('fos_message');
+        }
 
         $rootNode
             ->children()
